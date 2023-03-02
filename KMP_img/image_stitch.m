486f521da13dc629f74f55cfdb9b14c352a2800d
KMP_img <xpejsk00@vutbr.cz>
2023-03-02 13:14:40 +0100

    Signed-off-by: KMP_img <xpejsk00@vutbr.cz>

--- KMP_img/image_stitch.m
+++ KMP_img/image_stitch.m
@@ -6,11 +6,11 @@ output_panorama = color_im;
 obr = rgb2gray(color_im);
 
 %index cyklu
-cycle_idx = 1:1:size(image_splitted.J,2);
+cycle_idx = 1:1:size(image_splitted,2);
 
 
 %%
-for k = 1: size(image_splitted.J,2)
+for k = 1: size(image_splitted,2)
     %ulozeni parametru puvodniho obrazu
     corners_obr = detectORBFeatures(obr);
     [features_obr,valid_corners_obr] = extractFeatures(obr,corners_obr);
@@ -23,7 +23,7 @@ for k = 1: size(image_splitted.J,2)
     
     
     for j = cycle_idx(cycle_idx ~= 0)
-        im_current = rgb2gray(image_splitted.J{j});
+        im_current = rgb2gray(image_splitted{j});
         %harrisova vzdalenost
         corners_im_current = detectORBFeatures(im_current);
         [features_im_current,valid_corners_im_current] = extractFeatures(im_current,corners_im_current);
@@ -39,7 +39,7 @@ for k = 1: size(image_splitted.J,2)
     end
     
     cycle_idx(imidx) =0;
-    finim = rgb2gray(image_splitted.J{imidx});
+    finim = rgb2gray(image_splitted{imidx});
     %%
     %vyber n nejlepsich bodu
     npoints = 1;
@@ -71,7 +71,7 @@ for k = 1: size(image_splitted.J,2)
     %plot(im_current_points_pos(:,1),im_current_points_pos(:,2))
     
     %%
-    color_other = (image_splitted.J{imidx});
+    color_other = (image_splitted{imidx});
     %licovani
     %scaling=[V1.Location(pos_Start(1),2)-V2.Location(pos_test(1),2),V1.Location(pos_Start(1),1)-V2.Location(pos_test(1),1)];
     %V1 - valid_points startovního obrázku; V2-valid_points testovaného obr.
